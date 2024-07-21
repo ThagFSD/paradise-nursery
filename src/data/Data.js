b3e6f569387f6609247bd6cf77555bb236da1c03
root <root@ThangFSD.localdomain>
2024-07-21 19:45:58 +0700

    First commit

---
+++ src/data/Data.js
@@ -0,0 +1,133 @@
+export const socialIcon = [
+  {
+    icon: <i className="fab fa-facebook-f"></i>,
+    name: "facebook",
+  },
+  {
+    icon: <i className="fab fa-twitter"></i>,
+    name: "twitter",
+  },
+  {
+    icon: <i className="fab fa-youtube"></i>,
+    name: "youtube",
+  },
+  {
+    icon: <i className="fab fa-instagram"></i>,
+    name: "instagram",
+  },
+];
+
+export const products = [
+  {
+    id: 1,
+    category: "Flowers",
+    product_name: "Sunflower",
+    product_img: "../assets/img/sunflower.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    price: "4.99",
+  },
+
+  {
+    id: 2,
+    category: "Bayan Trees",
+    product_name: "Singapore Bayan",
+    product_img: "../assets/img/singaporeBayan.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    price: "2.99",
+  },
+
+  {
+    id: 3,
+    category: "Flowers",
+    product_name: "Tulip",
+    product_img: "../assets/img/tulip.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    price: "8.99",
+  },
+
+  {
+    id: 4,
+    category: "Flowers",
+    product_name: "White Rose",
+    product_img: "../assets/img/whiterose.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    price: "3.99",
+  },
+  {
+    id: 5,
+    category: "Cactus",
+    product_name: "Cactus",
+    product_img: "../assets/img/cactus1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    price: "3.99",
+  },
+
+  {
+    id: 6,
+    category: "Snake Plants",
+    product_name: "Viet Nam Snake Plant",
+    product_img: "../assets/img/snakeplt.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    price: "3.99",
+  },
+  {
+    id: 7,
+    category: "Snake Plants",
+    product_name: "Big Snake Plant",
+    product_img: "../assets/img/snakeplt2.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    price: "3.99",
+  },
+  {
+    id: 8,
+    category: "Cactus",
+    product_name: "Big Cactus",
+    product_img: "../assets/img/cactus2.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    price: "3.99",
+  },
+];
+export const slids = [
+  {
+    category: "Flowers",
+    slide_img: "../assets/img/hero-img-1.jpg",
+  },
+  {
+    category: "Plants",
+    slide_img: "../assets/img/hero-img-2.jpg",
+  },
+];
+export const features = [
+  {
+    id: 1,
+    icon: [<i className="fas fa-car-side fa-3x text-white"></i>],
+    title: "Free Shipping",
+    descript: "Free on order over $300",
+  },
+  {
+    id: 2,
+    icon: [<i className="fas fa-user-shield fa-3x text-white"></i>],
+    title: "Security Payment",
+    descript: "100% security payment",
+  },
+  {
+    id: 3,
+    icon: [<i className="fas fa-exchange-alt fa-3x text-white"></i>],
+    title: "30 Day Return",
+    descript: "30 day money guarantee",
+  },
+  {
+    id: 4,
+    icon: [<i className="fa fa-phone-alt fa-3x text-white"></i>],
+    title: "24/7 Support",
+    descript: "Support every time fast",
+  },
+];
